0d409185ba4aa9fced6ccce6044f4f7a9bdeb565
Vendip <vendipn@gmail.com>
2020-03-19 10:42:10 +0100

    these are my assignments

---
+++ Ifelse/Ifelse/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vendip
+ * Date: 3/19/2020
+ * Time: 10:32 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Ifelse
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+			if (10>5){
+				Console.WriteLine("You are Correct");			
+				}
+			
+			else{
+				Console.WriteLine("Getout!!!");
+			}
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ Whileloop/Whileloop/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vendip
+ * Date: 3/19/2020
+ * Time: 10:29 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Whileloop
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
